fb7552865bd91931a82af2a4159cf45afe957d00
avoforme <hophuongthanh028@gmail.com>
2026-04-23 18:10:10 -0400

    wrote reduce function

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,11 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"os"
+	"sort"
+)
+
 // doReduce does the job of a reduce worker: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
 // intermediate key/value pairs by key, calls the user-defined reduce function
@@ -25,10 +31,36 @@ func doReduce(
 	//       kvMap[kv.Key] = append(kvMap[kv.Key], kv.Value)
 	//     }
 	//
+
+	kvMap := make(map[string][]string)
+	for i := range nMap {
+		file, err := os.OpenFile(reduceName(jobName, i, reduceTaskNumber), os.O_RDONLY, 0644)
+		checkError(err)
+		decode := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if decode.Decode(&kv)!= nil {
+				break
+			}
+			
+			kvMap[kv.Key] = append(kvMap[kv.Key], kv.Value)
+			
+		}
+		file.Close()
+	}
+
 	// Step 2: Call reduceF for each unique key.
 	//   Collect all keys, sort them (sort.Strings), then for each key call
 	//   reduceF(key, kvMap[key]) to get the output value.
 	//
+    keys := make([]string, 0, len(kvMap))
+	for k := range kvMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	
+
 	// Step 3: Write output atomically using a temp file + os.Rename.
 	//   The final output filename is: mergeName(jobName, reduceTaskNumber)
 	//   See the Note in the project spec (Part A) for the required pattern:
@@ -39,5 +71,20 @@ func doReduce(
 	//     enc.Encode(KeyValue{key, reduceF(key, kvMap[key])})
 	//
 	// Use checkError to handle errors.
+
+	tempFile, err := os.CreateTemp("", "mr-reduce-*")
+	checkError(err)
+
+	encoder := json.NewEncoder(tempFile)
+
+	for _, key := range keys {
+		err := encoder.Encode(KeyValue{key, reduceF(key, kvMap[key])})
+		checkError(err)
+	}
+
+	tempFile.Close()
+	err = os.Rename(tempFile.Name(), mergeName(jobName, reduceTaskNumber))
+	checkError(err)
+
 
 }
